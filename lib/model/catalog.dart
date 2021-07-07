5b453c5137ae7cdb40bad8f0c7f28edd9f0aff7c
Harshad1999 <sonawaneharshad1999@gmail.com>
2021-07-07 11:27:21 +0530

    Day 11 complete

---
+++ lib/model/catalog.dart
@@ -0,0 +1,22 @@
+class item {
+  final num id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final product = [
+  item(
+    id: 1,
+    name: "Iphone 12 pro",
+    desc: "APple Phone 12th generation",
+    price: 999,
+    color: "#33505a",
+    image:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-12-pro-blue-hero?wid=940&hei=1112&fmt=png-alpha&qlt=80&.v=1604021661000",
+  )
+];
