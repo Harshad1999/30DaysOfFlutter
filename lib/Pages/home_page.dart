e334a0be0ae45205990ad027ccd51a007a25d9da
Harshad1999 <sonawaneharshad1999@gmail.com>
2021-07-08 00:55:57 +0530

    json decoding and mapping

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_practice_codepur/model/catalog.dart';
 import 'package:flutter_practice_codepur/widgets/drawer.dart';
 import 'package:flutter_practice_codepur/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final dummyList = List.generate(40, (index) => CatelogModel.items[0]);
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    localData();
+  }
+
+  localData() async {
+    final CatalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(CatalogJson);
+    var productsData = decodeData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(40, (index) => CatelogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(
