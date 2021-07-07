a93a34ef049d39e064c4e6031cbdc272ac59be9a
Harshad1999 <sonawaneharshad1999@gmail.com>
2021-07-08 00:25:17 +0530

    created card and listtile

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_codepur/model/catalog.dart';
 import 'package:flutter_practice_codepur/widgets/drawer.dart';
+import 'package:flutter_practice_codepur/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
+  final dummyList = List.generate(40, (index) => CatelogModel.items[0]);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,9 +13,15 @@ class HomePage extends StatelessWidget {
           "Catalog App",
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome To 30 Days of Flutter"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
